58c9080392f405db84a66a49a425aaa4f97dff32
GopalVerma1303 <vgopaldps@gmail.com>
2024-12-29 14:21:56 +0530

    update: nvim config

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -88,10 +88,55 @@ return {
 	},
 
 	{
-		"nvim-cmp",
-		dependencies = { "hrsh7th/cmp-emoji" },
+		"hrsh7th/nvim-cmp",
+		enabled = true,
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-emoji",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
 		opts = function(_, opts)
 			table.insert(opts.sources, { name = "emoji" })
 		end,
 	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	enabled = true,
+	-- 	event = "InsertEnter",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-buffer",
+	-- 		"hrsh7th/cmp-path",
+	-- 		"hrsh7th/cmp-emoji",
+	-- 		"L3MON4D3/LuaSnip",
+	-- 		"saadparwaiz1/cmp_luasnip",
+	-- 	},
+	-- 	opts = function(_, opts)
+	-- 		local cmp = require("cmp")
+	--
+	-- 		opts.mapping = cmp.mapping.preset.insert({
+	-- 			["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+	-- 			["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+	-- 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	-- 			["<C-f>"] = cmp.mapping.scroll_docs(4),
+	-- 			["<C-Space>"] = cmp.mapping.complete(),
+	-- 			["<C-e>"] = cmp.mapping.abort(),
+	-- 			["<CR>"] = cmp.mapping.confirm({ select = true }),
+	-- 		})
+	--
+	-- 		opts.sources = cmp.config.sources({
+	-- 			{ name = "nvim-lsp" },
+	-- 			{ name = "luasnip" },
+	-- 			{ name = "buffer" },
+	-- 			{ name = "path" },
+	-- 			{ name = "emoji" },
+	-- 		})
+	--
+	-- 		return opts
+	-- 	end,
+	-- },
 }
